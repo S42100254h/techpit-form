d49ddfb0e4c168cdbce4679a2895a08bf884c8c8
yuki sawabe <sawabe0v0@gmail.com>
2021-11-23 09:45:37 +0900

    update Address.tsx

--- front/src/components/Address.tsx
+++ front/src/components/Address.tsx
@@ -6,6 +6,7 @@ import { RootState } from "../domain/entity/rootState";
 import { Address as IAddress } from "../domain/entity/address";
 import useStyles from "./styles";
 import profileActions from "../store/profile/actions";
+import { isPostalcode } from "../domain/services/address";
 
 const Address = () => {
   const classes = useStyles();
@@ -15,6 +16,14 @@ const Address = () => {
   const handleAddressChange = (member: Partial<IAddress>) => {
     dispatch(profileActions.setAddress(member));
   };
+  
+  const handlePostalcodeChange = (code: string) => {
+    if(!isPostalcode(code)) {
+      return;
+    } else {
+      dispatch(profileActions.setAddress({ postalcode: code }));
+    }
+  };
 
   return (
     <>
@@ -23,7 +32,7 @@ const Address = () => {
         className={classes.formField}
         label={PROFILE.ADDRESS.POSTALCODE}
         value={profile.address.postalcode}
-        onChange={e => handleAddressChange({ postalcode: e.target.value })}
+        onChange={e => handlePostalcodeChange(e.target.value)}
       />
       <TextField
         fullWidth
