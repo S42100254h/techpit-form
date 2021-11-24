54e323eb203e940700450aa5974731ab9b34537f
yuki sawabe <sawabe0v0@gmail.com>
2021-11-24 17:52:13 +0900

    connect Career.tsx to redux

--- front/src/components/Career.tsx
+++ front/src/components/Career.tsx
@@ -1,57 +1,81 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../domain/entity/rootState";
+import { Career as ICareer } from "../domain/entity/career";
+import profileActions from "../store/profile/actions";
 import { TextField, Grid, InputLabel, Typography } from "@material-ui/core";
 import { PROFILE } from "../domain/services/profile";
 import useStyles from "./styles";
 
 const Career = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const careers = useSelector((state: RootState) => state.profile.careers);
+  
+  const handleChange = (member: Partial<ICareer>, i: number) => {
+    dispatch(profileActions.setCareer({ career: member, index: i }));
+  };
   
   return (
     <>
-      <Typography variant="h5" component="h3" className={classes.title}>
-        職歴
-      </Typography>
-      <TextField
-        className={classes.formField}
-        fullWidth
-        label={PROFILE.CAREERS.COMPANY}
-      />
-      <TextField
-        className={classes.formField}
-        fullWidth
-        label={PROFILE.CAREERS.POSITION}
-      />
-      <div className={classes.careerSpan}>
-        <InputLabel shrink>{PROFILE.CAREERS.SPAN}</InputLabel>
-        <Grid
-          container
-          spacing={1}
-          alignContent="space-between"
-          alignItems="center"
-        >
-          <Grid item xs={5}>
-            <TextField
-              fullWidth
-              type="month"
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-          <Grid item xs={2}>
-            <Typography align="center">〜</Typography>
-          </Grid>
-          <Grid item xs={5}>
-            <TextField
-              fullWidth
-              type="month"
-              InputLabelProps={{
-                shrink: true
-              }}
-            />
-          </Grid>
-        </Grid>
-      </div>
+      {careers.map((c, i) => (
+        <Fragment key={i}>
+          <Typography variant="h5" component="h3" className={classes.title}>
+            職歴 {i + 1}
+          </Typography>
+          <TextField
+            className={classes.formField}
+            fullWidth
+            label={PROFILE.CAREERS.COMPANY}
+            value={c.company}
+            onChange={e => handleChange({ position: e.target.value }, i)}
+          />
+          <TextField
+            className={classes.formField}
+            fullWidth
+            label={PROFILE.CAREERS.POSITION}
+            value={c.position}
+            onChange={e => handleChange({ position: e.target.value }, i)}
+          />
+          <div className={classes.careerSpan}>
+            <InputLabel shrink>{PROFILE.CAREERS.SPAN}</InputLabel>
+            <Grid
+              container
+              spacing={1}
+              alignContent="space-between"
+              alignItems="center"
+            >
+              <Grid item xs={5}>
+                <TextField
+                  fullWidth
+                  type="month"
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  value={c.startAt}
+                  onChange={e => handleChange({ position: e.target.value }, i)}
+                />
+              </Grid>
+              <Grid item xs={2}>
+                <Typography align="center">〜</Typography>
+              </Grid>
+              <Grid item xs={5}>
+                <TextField
+                  fullWidth
+                  type="month"
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  value={c.endAt}
+                  onChange={e => handleChange({ position: e.target.value }, i)}
+                />
+              </Grid>
+            </Grid>
+          </div>
+        </Fragment>
+      ))}
     </>
   );
 };
+
+export default Career;
